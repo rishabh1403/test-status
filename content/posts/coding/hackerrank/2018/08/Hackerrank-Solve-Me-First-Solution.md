267f6f03cfe381e8e11a62dc91b9bae0029c12ca
Rishabh Jain <mailrishabh1403@gmail.com>
2018-08-21 16:31:27 +0530

    add support for backup

---
+++ content/posts/coding/hackerrank/2018/08/Hackerrank-Solve-Me-First-Solution.md
@@ -0,0 +1,10 @@
+---
+title: "Hackerrank Solve Me First Solution"
+description: "Hackerrank Solve Me First Solution"
+author: "Rishabh Jain"
+tags: []
+categories: []
+date: 2018-08-21T12:32:09+05:30
+draft: true
+---
+
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,17 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+#create backup dir if not present
+mkdir publicbak
+
+#create a backup
+echo -e "\033[0;32mMaking a backup copy...\033[0m"
+cp -v -r public/* publicbak/
+
+#clean up public
+echo -e "\033[0;32mDeleting old copy...\033[0m"
+rm -rf public/*
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
